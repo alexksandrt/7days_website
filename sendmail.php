b877144f4a50a575accd657619be9cbe529b5f65
alexksandrt <83001059+alexksandrt@users.noreply.github.com>
2023-06-22 08:39:17 +0600

    first commint from developers

---
+++ sendmail.php
@@ -0,0 +1,43 @@
+﻿<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('info@example.com', 'Заявка с сайта');
+	//Кому отправить
+	$mail->addAddress('info@example.com');
+	//Тема письма
+	$mail->Subject = 'Заявка с сайта';
+
+	//Тело письма
+	
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['tel']))){
+		$body.='<p><strong>Телефон:</strong> '.$_POST['tel'].'</p>';
+	}
+	
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
